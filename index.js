1dd73048bdcb4d807df7d765d190a66e69be0cd9
Pradas137 <pradas137@gmail.com>
2021-01-12 17:26:06 +0100

    register

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser =require('body-parser')
+const app = express()
+const port = 3001
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine','ejs')
+
+/*
+app.get('/', (req,res) => {
+    res.send('<form method="post">\
+        <input name="nom" placeholder="el teu nom...">\
+        <input type="submit"/>\
+        </form>')
+})
+*/
+
+if (typeof localStorage === "undefined" || localStorage===null) {
+    var localStorage=require('node-localstorage').LocalStorage;
+    localStorage= new LocalStorage('./scratch');
+}
+
+localStorage.setItem("adrian","adrian")
+localStorage.setItem("pili","pili")
+
+app.get('/', (req,res) => {
+    res.render('home')
+
+})
+
+app.post("/",function(req,res){
+    var nom=req.body.nom;
+    res.send("Hola "+nom);
+})
+
+app.get('/login', function(req,res) {
+    res.render('login',{})
+})
+/*
+var dades = {
+    "adrian":"adrian",
+    "pili":"pili"
+}
+*/
+app.post('/login', function(req,res) {
+    var usuari=req.body.nom;
+    var contrasenya=req.body.contrasenya;
+
+    var contra2 = localStorage.getItem(usuari)
+    console.log("Contra2="+contra2)
+
+    if ( contra2==contrasenya ) {
+        res.send("OK")
+    } else {
+        res.send("NotOK")
+    }
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
