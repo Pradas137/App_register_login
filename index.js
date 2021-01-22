e55b4db7bec87aee1ef796b055448ca99c390de6
Pradas137 <pradas137@gmail.com>
2021-01-22 18:43:25 +0100

    register-login-logout

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//$ curl http://localhost:3001/api/login/alex/alex
+//$ curl -X POST localhost:3001/api/login -d '{"nom":"alex","pass":"alex"}' -H "Content-Type: application/json"
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs');
@@ -40,7 +43,7 @@ app.post("/login", function(req, res){
     };
   }
   if (exist == false) {
-    res.send("Usuari i/o contrasenya incorrectes. Torna a intentar-ho<br><a href='./login'>Inicia sessió</a>")
+    res.send("Usuari i/o contrasenya incorrectes. Torna a intentar-ho<br>s")
   }
 })
 
@@ -49,18 +52,18 @@ app.get("/register", function(req, res){
 })
 
 app.post("/register", function(req, res){
-  var usuari = req.body.usuari;
+  var usuari = req.body.nom;
   var contrasenya = req.body.contrasenya;
   var token = "";
   var data = {password: contrasenya, token: token};
 
   localStorage.setItem(usuari, JSON.stringify(data));
-  res.send("El usuari ha estat registrat correctament <br><a href='./login'>Inicia sessió</a>")
+  res.send("Registro Correcto<br>")
 })
 
 app.post("/api/login", function(req, res){
-  var usuari = req.body.user;
-  var contrasenya = req.body.pass;
+  var usuari = req.body.nom;
+  var contrasenya = req.body.contrasenya;
   var exist = false;
   for (var i = 0; i < localStorage.length; i++) {
     if (usuari == localStorage.key(i)){
@@ -70,13 +73,28 @@ app.post("/api/login", function(req, res){
         var token = require('crypto').randomBytes(64).toString('hex');
         var data = {password: contrasenya, token: token};
         localStorage.setItem(usuari, JSON.stringify(data));
-        res.send("WILLKOMMEN")
+        res.send("Hola Bienbenido")
       }
     };
   }
   if (exist == false) {
-    res.send("Usuari i/o contrasenya incorrectes. Torna a intentar-ho")
+    res.send("ERROR: Usuari o contrasenya.")
   }
+})
+
+app.post("/api/logout", function(req, res){
+  var token = req.body.token;
+  for (var i = 0; i < localStorage.length; i++) {
+    user = localStorage.key(i);
+    user_data = JSON.parse(localStorage.getItem(user));
+    if (token == user_data['token']){
+      new_data = {password: user_data['password'], token: ""};
+      localStorage.setItem(user, JSON.stringify(new_data));
+      res.send('Cerrando sesion.');
+    } else {
+      res.send('Error al cerrar sesion.');
+    }
+  };
 })
 
 app.listen(port, () => {
